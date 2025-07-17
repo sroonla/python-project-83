57351889f27374b2dab8b963f9cd600054e7d2f5
sroonla <sr0.onlady@mail.ru>
2025-07-18 06:03:50 +0900

    1

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,28 @@
 import pytest
-from page_analyzer.db import get_connection, normalize_url, is_valid_url
+from page_analyzer.db import get_connection, normalize_url, is_valid_url, add_url, get_url_by_id, get_url_by_name, add_url_check, get_url_checks
 
+def test_add_url():
+    url = "https://example.com"
+    url_id = add_url(url)
+    assert url_id is not None
+
+    url_from_db = get_url_by_id(url_id)
+    assert url_from_db["name"] == url
+
+    same_url = get_url_by_name(url)
+    assert same_url["id"] == url_id
+
+def test_add_url_check():
+    url = "https://example.com"
+    url_id = add_url(url)
+
+    check_id = add_url_check(url_id, 200, "Test Title", "Test H1", "Test Description")
+    assert check_id is not None
+
+    checks = get_url_checks(url_id)
+    assert len(checks) == 1
+    assert checks[0]["status_code"] == 200
+    
 def test_db_connection():
     conn = get_connection()
     assert conn is not None
