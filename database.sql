c105f9b1cf5ede7c3b8283637d36cc5ee8e8d14c
sroonla <sr0.onlady@mail.ru>
2025-08-06 16:02:54 +0900

    4

--- database.sql
+++ database.sql
@@ -1,17 +1,15 @@
--- Создание таблицы для URL
 CREATE TABLE IF NOT EXISTS urls (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at date
 );
 
--- Создание таблицы для проверок URL
 CREATE TABLE IF NOT EXISTS url_checks (
-    id SERIAL PRIMARY KEY,
-    url_id INTEGER NOT NULL REFERENCES urls(id),
-    status_code INTEGER,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+    status_code int, 
+    h1 text, 
+    title text, 
+    description text,
+    created_at date
 );
